b962a04807ab9f8dcae38078bccec5f114a84299
SilvioAlves98 <silvioluizmilanialves@gmail.com>
2022-08-02 11:29:05 -0300

    Atualizações...

---
+++ src/main/java/ExercicioControleDeFluxo3.java
@@ -0,0 +1,11 @@
+public class ExercicioControleDeFluxo3 {
+    public static void main(String[] args) {
+        int valor = 50000;
+        for (int i=0; i<valor; i++){
+            if(i%2!=0){
+                System.out.println(i);
+            }
+        }
+
+    }
+}
