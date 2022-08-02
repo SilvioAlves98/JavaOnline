69a2e3dcccb0b03baedf5038b704f7c9952912a7
SilvioAlves98 <silvioluizmilanialves@gmail.com>
2022-08-02 08:59:17 -0300

    Initial commit

---
+++ src/main/java/ImprimindoVariaveis.java
@@ -0,0 +1,6 @@
+public class ImprimindoVariaveis {
+    public static void main(String[] args){
+        int idadeDosPaisParaCadastro = 10;
+        System.out.println("A idade e: "+idadeDosPaisParaCadastro);
+    }
+}
---
+++ src/main/java/RelatorioPendencia.java
@@ -0,0 +1,10 @@
+public class RelatorioPendencia {
+    public static void main(String[] args){
+        String nome ="Goku";
+        String endereco ="439 East District";
+        String telefone = "(1)1234-5689";
+
+        System.out.println("O "+nome+" domiciliado no endereco "+endereco+" e telefone "+telefone+" Nao possui pendencias");
+
+    }
+}
