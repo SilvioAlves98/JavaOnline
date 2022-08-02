f0a8e9603a809bf39b53b1222081c6bfa6dd12bd
SilvioAlves98 <silvioluizmilanialves@gmail.com>
2022-08-02 10:45:13 -0300

    Atualizações...

---
+++ src/main/java/ExercicioControleFluxo2.java
@@ -0,0 +1,9 @@
+public class ExercicioControleFluxo2 {
+    public static void main(String[] args) {
+        byte dia = 1;
+        switch (dia){
+            case 1: case 7:
+                System.out.println("fds"); break;
+        }
+    }
+}
